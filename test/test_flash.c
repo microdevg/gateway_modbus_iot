277c21cf1427a055c73b2c9ede210006161984ff
German Velardez <germanvelardez16@gmail.com>
2024-11-19 23:36:30 -0300

    [Test configurado correctamente]

--- test/test_flash.c
+++ test/test_flash.c
@@ -1,6 +1,8 @@
-#include "flash_mem.h"
 #include "unity.h"
-
+#include "flash_mem.h"
+#include "mock_esp_err.h"
+#include "mock_nvs.h"
+#include "mock_nvs_flash.h"
 
 
 
@@ -14,6 +16,20 @@ void tearDown(void) {
 }
 
 void test_flash_mem_init(void){
+
+char id[20]; 
+char pass[20];
+nvs_set_str_IgnoreAndReturn(ESP_OK);
+nvs_set_str_IgnoreAndReturn(ESP_OK);
+nvs_commit_ExpectAnyArgsAndReturn(ESP_OK);
+nvs_close_Stub(ESP_OK);
+nvs_open_IgnoreAndReturn(ESP_OK);
+nvs_close_ExpectAnyArgs();
+nvs_flash_init_IgnoreAndReturn(ESP_OK);
+esp_err_t  err = flash_mem_save( id, pass);
+TEST_ASSERT_EQUAL(ESP_OK,err);
+
+
 
 
 }

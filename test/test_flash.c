5f87e9b3a19bf2cca28a0f6f9d25a3a038bf66d1
German Velardez <germanvelardez16@gmail.com>
2024-11-19 22:32:37 -0300

    Agregue ceedling, falta configurar rutas y agregar .h's

---
+++ test/test_flash.c
@@ -0,0 +1,19 @@
+#include "flash_mem.h"
+#include "unity.h"
+
+
+
+
+
+void setUp(void) {
+    // Código que se ejecuta antes de cada test (si es necesario)
+}
+
+void tearDown(void) {
+    // Código que se ejecuta después de cada test (si es necesario)
+}
+
+void test_flash_mem_init(void){
+
+
+}
